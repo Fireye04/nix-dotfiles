b06b55107ff5675b561bdc7b05afa6fa55cf3495
Fireye <codekai16@gmail.com>
2025-08-19 02:42:08 -0700

    rev

--- root/default.nix
+++ root/default.nix
@@ -89,7 +89,7 @@
 
 	programs.nix-ld = {
 		enable = true;
-		libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [xorg.libSM]);
+		libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [xorg.libSM xorg.libICE]);
 	};
 	programs.steam.enable = true;
 	# Some programs need SUID wrappers, can be configured further or are
