81fcd159b654200fd7e820f39f811b8f918970df
Fireye <codekai16@gmail.com>
2025-09-06 19:15:22 -0700

    rev

--- root/services/nvim.nix
+++ root/services/nvim.nix
@@ -140,7 +140,7 @@
 					servers = {
 						java = ''
 							function(callback, config)
-							  M.execute_command({command = 'vscode.java.startDebugSession'}, function(err0, port)
+							  execute_command({command = 'vscode.java.startDebugSession'}, function(err0, port)
 							    assert(not err0, vim.inspect(err0))
 							    callback({ type = 'server'; host = '127.0.0.1'; port = port; })
 							  end)
