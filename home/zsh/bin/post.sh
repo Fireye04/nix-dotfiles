47f4c15f0d000b838ff64127aa326a2eda49aec2
Fireye <codekai16@gmail.com>
2025-09-12 17:03:36 -0700

    rev

--- home/zsh/bin/post.sh
+++ home/zsh/bin/post.sh
@@ -2,9 +2,9 @@ cd ~/Projects/websites/coffee
 
 hugo new "changelog/new-content.md"
 if [ -z "$2"];then
-	hugo new "blog/" + $1
+	hugo new "blog/$1"
 	nvim ./content/blog/$1
 else
-	hugo new $2 + "/" $1;
+	hugo new "$2/$1";
 	nvim ./content/$2/$1
 fi
