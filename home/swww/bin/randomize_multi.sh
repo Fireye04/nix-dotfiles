9f52ca970b2b05ab6a1b16993a1c3c1ed137770c
Fireye <codekai16@gmail.com>
2025-10-06 12:05:24 -0700

    rev

--- home/swww/bin/randomize_multi.sh
+++ home/swww/bin/randomize_multi.sh
@@ -1,4 +1,8 @@
-DEFAULT_INTERVAL=60 # In seconds
+#!/bin/sh
+# For each display, changes the wallpaper to a randomly chosen image in
+# a given directory at a set interval.
+
+DEFAULT_INTERVAL=300 # In seconds
 
 if [ $# -lt 1 ] || [ ! -d "$1" ]; then
 	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
@@ -18,11 +22,11 @@ while true; do
 	done \
 	| sort -n | cut -d':' -f2- \
 	| while read -r img; do
-		for d in $(swww query | grep -Po "^[^:]+"); do # see swww-query(1)
+		for d in $(swww query | awk '{print $2}' | sed s/://); do # see swww-query(1)
 			# Get next random image for this display, or re-shuffle images
 			# and pick again if no more unused images are remaining
 			[ -z "$img" ] && if read -r img; then true; else break 2; fi
-			swww img --resize "$RESIZE_TYPE" --outputs "$d" "$img" -t random
+			swww img --resize "$RESIZE_TYPE" --outputs "$d" "$img"
 			unset -v img # Each image should only be used once per loop
 		done
 		sleep "${2:-$DEFAULT_INTERVAL}"
