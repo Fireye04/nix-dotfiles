b917053266fe9887a6d5da8f9b59a1938638ee57
Fireye <codekai16@gmail.com>
2025-10-15 20:25:48 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -44,6 +44,7 @@
 		xdg-desktop-portal-gnome
 		libgnome-keyring
 		nautilus
+		udisks
 
 		hyfetch
 		ranger
@@ -202,7 +203,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''							
+			text = ''					
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
