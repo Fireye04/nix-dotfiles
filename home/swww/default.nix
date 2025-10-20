2b2191fd56554b434efa1b5f0438b8983c2b7739
Fireye <codekai16@gmail.com>
2025-10-20 14:32:46 -0700

    rev

--- home/swww/default.nix
+++ home/swww/default.nix
@@ -2,10 +2,16 @@
 	config,
 	pkgs,
 	...
-}: {
+}: let
+	file =
+		builtins.fetchurl {
+			url = "https://github.com/LGFae/swww/blob/main/example_scripts/swww_randomize_multi.sh";
+			sha256 = "";
+		};
+in {
 	home.file = {
 		".config/swww/bin/randomize_multi.sh" = {
-			source = ./bin/randomize_multi.sh;
+			source = file;
 			executable = true;
 		};
 	};
