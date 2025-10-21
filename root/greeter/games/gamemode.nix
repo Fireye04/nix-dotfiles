c1707ed95afe1cbdb053c70e1b73b1c7d705cc5c
Fireye <codekai16@gmail.com>
2025-10-21 13:34:53 -0700

    rev

--- root/greeter/games/gamemode.nix
+++ root/greeter/games/gamemode.nix
@@ -1,15 +1,2 @@
-{
-	stdenv,
-	system,
-	pkgs,
-}:
-stdenv.mkDerivation {
-	inherit system;
-	name = "bash-gamemode";
-	builder = "${pkgs.bash}/bin/bash";
-	args = ["-c" ''touch "$out"''];
-	outputs = ["out"];
-	shellHook = ''
-		gamescope -e --adaptive-sync -- steam -gamepadui"
-	'';
-}
+{pkgs}:
+pkgs.writeShellScript "bash-gamemode" ''gamescope -e --adaptive-sync -- steam -gamepadui"''
