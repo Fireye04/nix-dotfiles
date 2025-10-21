6c78dba89f6a92a5e85de949c9fb0d9ee3f7e870
Fireye <codekai16@gmail.com>
2025-10-21 14:07:08 -0700

    rev

--- root/greeter/games/no-mans-sky.nix
+++ root/greeter/games/no-mans-sky.nix
@@ -2,4 +2,4 @@
 	stdenv,
 	pkgs,
 }:
-pkgs.writeShellScriptBin "bash-gamemode" ''gamescope -e -r 59 --adaptive-sync --hdr-enabled -- steam steam://rungameid/275850''
+pkgs.writeShellScriptBin "no-mans-sky" ''gamescope -e -r 59 --adaptive-sync --hdr-enabled -- steam steam://rungameid/275850''
