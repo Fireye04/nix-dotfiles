bdf95e4a4600d797f6c5dcecc51548fed6234539
Fireye <codekai16@gmail.com>
2025-10-21 12:29:48 -0700

    rev

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -6,6 +6,6 @@
 		# bashrcExtra = "amixer sset Master unmute\n
 		# 	amixer sset Speaker unmute\n
 		# 	amixer sset Headphone unmute";
-		shellAliases = "gamescope -e --adaptive-sync -- steam -gamepadui";
+		shellAliases ={GS = "gamescope -e --adaptive-sync -- steam -gamepadui"};
 	};
 }
