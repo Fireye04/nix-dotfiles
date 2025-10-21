99a728683c833b8bd3f35bb005cb0ed1aeaf171e
Fireye <codekai16@gmail.com>
2025-10-20 17:17:06 -0700

    rev

--- home/tofi.nix
+++ home/tofi.nix
@@ -183,8 +183,8 @@
 			### Window theming
 			#
 			# Width and height of the window. Can be pixels or a percentage.
-			width = 500;
-			height = 500;
+			width = "30%";
+			height = "100%";
 
 			# Window background color
 			background-color = "#1B1D1E";
