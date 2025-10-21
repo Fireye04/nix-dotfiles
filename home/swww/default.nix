f74b9628e27aff8b80e585959e2ca0327e4c41b5
Fireye <codekai16@gmail.com>
2025-10-20 18:18:05 -0700

    rev

--- home/swww/default.nix
+++ home/swww/default.nix
@@ -4,7 +4,7 @@
 	...
 }: let
 	file =
-		pkgs.fetchFromGithub {
+		pkgs.fetchFromGitHub {
 			url = "https://github.com/LGFae/swww/blob/main/example_scripts/swww_randomize_multi.sh";
 			sha256 = "sha256:1mxqrxvyzg54xz2h453pk7pska3ggxq1za4flwladgqfphj8lnxc";
 		};
