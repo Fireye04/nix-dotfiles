d78b019e0684157b9b2ed10b5b2cf5fff01ebce0
Fireye <codekai16@gmail.com>
2025-10-21 13:38:34 -0700

    rev

--- root/greeter/games/gamemode.nix
+++ root/greeter/games/gamemode.nix
@@ -1,2 +1,8 @@
-{pkgs}:
-pkgs.writeShellScript "bash-gamemode" ''gamescope -e --adaptive-sync -- steam -gamepadui''
+{
+	stdenv,
+	pkgs,
+}:
+stdenv.mkDerivation {
+	name = "bash-gamemode";
+	src = pkgs.writeShellScript "bash-gamemode" ''gamescope -e --adaptive-sync -- steam -gamepadui'';
+}
