5f52f14f7fac88f4ac33f2f7ce624b13337bf076
Fireye <codekai16@gmail.com>
2025-10-20 17:31:17 -0700

    rev

--- home/tofi.nix
+++ home/tofi.nix
@@ -33,7 +33,7 @@
 			font = "/usr/share/fonts/noto/NotoSansMono-Regular.ttf";
 
 			# Point size of text.
-			font-size = 50;
+			font-size = 30;
 
 			# Comma separated list of OpenType font feature settings to apply,
 			# if supported by the chosen font. The format is similar to the CSS
