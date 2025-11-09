2283d5797ed88fb8d1d231aa3cf1fbd770cf36cf
Fireye <codekai16@gmail.com>
2025-11-09 12:46:02 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -113,7 +113,7 @@
 				}))
 		godotPackages.export-template-mono
 
-		lutris-free
+		pkgs-stable.lutris
 		mangohud
 
 		# archives
@@ -216,7 +216,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''					
+			text = ''				
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
