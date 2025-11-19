4aface72f9f5f68a6287c8d4c7cde092a2c5e959
Fireye <codekai16@gmail.com>
2025-11-18 18:17:43 -0700

    rev

--- root/services/nvim.nix
+++ root/services/nvim.nix
@@ -193,6 +193,7 @@
 				gdscript.enable = true;
 				clangd.enable = true;
 				ccls.enable = true;
+				quick_lint_js.enable = true;
 				asm_lsp = {
 					enable = true;
 					package = pkgs.asm-lsp;
