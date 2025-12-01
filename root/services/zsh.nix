26841851cb7235a7879eeef265a9232c94931ab4
Fireye <codekai16@gmail.com>
2025-11-30 20:43:56 -0700

    rev

--- root/services/zsh.nix
+++ root/services/zsh.nix
@@ -11,13 +11,13 @@
 			ninit = "cd /etc/nixos;nvim .";
 			update = "sudo nixos-rebuild switch";
 			gupdate = "git add --all;git commit -m 'rev';git push;sudo nixos-rebuild switch";
-            nixupdate = "git branch -D update;git checkout -b update;git add --all;git commit -m 'update';git push;nix flake update;sudo nixos-rebuild switch"
+			nixupdate = "git branch -D update;git checkout -b update;git add --all;git commit -m 'update';git push;nix flake update;sudo nixos-rebuild switch";
 			gaa = "git add --all";
 			gc = "git commit";
 			gp = "git push";
 			gst = "git status";
-            gco = "git checkout";
-            gcb = "git checkout -b";
+			gco = "git checkout";
+			gcb = "git checkout -b";
 
 			# Games
 			no-mans-sky = "gamemoderun gamescope -- steam steam://rungameid/275850 | tee Runtime-logs.txt";
