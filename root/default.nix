a2faa59257d928e63e291cc7a745ced3ed0b98e2
Fireye <codekai16@gmail.com>
2025-12-04 18:27:09 -0700

    rev

--- root/default.nix
+++ root/default.nix
@@ -7,7 +7,6 @@
 	pkgs-stable,
 	inputs,
 	options,
-	stdenv,
 	...
 }: {
 	imports = [
@@ -208,5 +207,5 @@
 	# this value at the release version of the first install of this system.
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	stdenv.hostPlatform.system.stateVersion = "25.11"; # Did you read the comment?
+	system.stateVersion = "25.05"; # Did you read the comment?
 }
