f3367a1da6814b03e5b9c79c4932fabf96d49a17
Fireye <codekai16@gmail.com>
2025-12-22 22:09:05 -0700

    rev

--- root/utils/waterfox/default.nix
+++ root/utils/waterfox/default.nix
@@ -1,6 +1,7 @@
 {
 	stdenv,
 	fetchurl,
+	makeDesktopItem,
 }:
 stdenv.mkDerivation {
 	version = "6.6.7";
@@ -10,10 +11,13 @@ stdenv.mkDerivation {
 			url = "https://cdn.waterfox.com/waterfox/releases/6.6.7/Linux_x86_64/waterfox-6.6.7.tar.bz2";
 			hash = "sha256-/R2rQkYPJ8boC/hHK39UuIkKyAHjaW+t7s5aaWZYTTI=";
 		};
-	installPhase = ''
-		mkdir -p $out/bin
-		chmod +x $out
-		    echo '#!/bin/bash' > $out
-		    echo 'echo "Hello, World!"' >> $out
-	'';
+	# installPhase = ''
+	# '';
+	desktopItems = [
+		(makeDesktopItem {
+				name = "waterfox";
+				desktopName = "waterfox";
+				exec = "$out/waterfox";
+			})
+	];
 }
