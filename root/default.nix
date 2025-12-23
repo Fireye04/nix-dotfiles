f8bae55f69b61ad5722c43f78c5e08125c19dcfd
Fireye <codekai16@gmail.com>
2025-12-23 12:51:31 -0700

    rev

--- root/default.nix
+++ root/default.nix
@@ -7,6 +7,7 @@
 	pkgs-stable,
 	inputs,
 	options,
+	lib,
 	...
 }: {
 	imports = [
@@ -174,6 +175,24 @@
 		liberation_ttf
 		font-awesome
 	];
+
+	services.flatpak.enable = true;
+
+	services.flatpak.remotes =
+		lib.mkOptionDefault [
+			{
+				name = "flathub-beta";
+				location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+			}
+		];
+
+	services.flatpak.update.auto.enable = false;
+	services.flatpak.uninstallUnmanaged = false;
+	# Add flatpaks here
+	services.flatpak.packages = [
+		"net.waterfox.waterfox"
+	];
+
 	virtualisation.docker.enable = true;
 
 	programs.nix-ld = {
