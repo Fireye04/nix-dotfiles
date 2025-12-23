609c3e5ef9fa4395f80ce04bdfaa454fa5315f14
Fireye <codekai16@gmail.com>
2025-12-22 22:00:25 -0700

    rev

--- root/utils/waterfox/default.nix
+++ root/utils/waterfox/default.nix
@@ -2,9 +2,9 @@
 	stdenv,
 	fetchurl,
 }:
-stdenv.mkDerivation rec {
-	name = "waterfox-${version}";
+stdenv.mkDerivation {
 	version = "6.6.7";
+	name = "waterfox";
 	src =
 		fetchurl {
 			url = "https://cdn.waterfox.com/waterfox/releases/6.6.7/Linux_x86_64/waterfox-6.6.7.tar.bz2";
@@ -12,5 +12,8 @@ stdenv.mkDerivation rec {
 		};
 	installPhase = ''
 		mkdir -p $out/bin
-		chmod +x $out'';
+		chmod +x $out
+		    echo '#!/bin/bash' > $out
+		    echo 'echo "Hello, World!"' >> $out
+	'';
 }
