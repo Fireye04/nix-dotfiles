7436804f6e44572c8ffb1ffed5cf4cef7fb6bbbd
Fireye <codekai16@gmail.com>
2025-12-22 21:54:50 -0700

    rev

--- root/utils/waterfox/default.nix
+++ root/utils/waterfox/default.nix
@@ -10,6 +10,7 @@ stdenv.mkDerivation rec {
 			url = "https://cdn.waterfox.com/waterfox/releases/6.6.7/Linux_x86_64/waterfox-6.6.7.tar.bz2";
 			hash = "sha256-/R2rQkYPJ8boC/hHK39UuIkKyAHjaW+t7s5aaWZYTTI=";
 		};
-	installPhase = ''mkdir -p $out/bin'';
-	startScript = ''echo $out;echo ${src}'';
+	installPhase = ''
+		mkdir -p $out/bin
+		chmod +x $out'';
 }
