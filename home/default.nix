2616fae5aba8bfc64fda7fa82191ed61ca74c6f5
Fireye <codekai16@gmail.com>
2026-01-01 19:41:12 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -83,6 +83,7 @@
 		helvum
 		# inputs.zen-browser.packages.${system}.default
 		inputs.fix-python.packages.${system}.default
+		inputs.colmena.packages.${system}.default
 		inputs.quickshell.packages.${system}.default
 		inputs.nix-citizen.packages.${system}.rsi-launcher
 		niri-unstable
@@ -192,7 +193,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''					
+			text = ''				
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
