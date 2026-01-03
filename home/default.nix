99e2fdecdc444c6df6eb65493eaea429172e35cf
Fireye <codekai16@gmail.com>
2026-01-03 13:36:49 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -103,6 +103,7 @@
 		wl-clipboard
 		nirius
 		nmap
+		ardour
 
 		# Communication
 		thunderbird
@@ -195,7 +196,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''						
+			text = ''					
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
