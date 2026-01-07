4389a910eb915dca1f98236897b9ccc300863f2d
Fireye <codekai16@gmail.com>
2026-01-06 23:28:36 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -68,6 +68,7 @@
 		tor-browser
 		yt-dlp
 		pkgs-stable.etherpad-lite
+		vlc
 
 		hyfetch
 		anki
@@ -198,7 +199,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''						
+			text = ''					
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
