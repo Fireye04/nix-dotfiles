86a4ef6c23f0cff9331d27bbc2ab12d83385b130
Fireye <codekai16@gmail.com>
2026-01-20 16:21:41 -0700

    rev

--- root/default.nix
+++ root/default.nix
@@ -79,7 +79,6 @@
 		description = "fireye";
 		extraGroups = ["networkmanager" "wheel" "docker"];
 		packages = with pkgs; [
-			(callPackage ./utils/nvimunity {})
 		];
 	};
 
@@ -121,6 +120,7 @@
 		bash
 		jq
 		# (callPackage ./utils/waterfox {})
+		(callPackage ./utils/nvimunity {})
 		(callPackage ./greeter/games/gamemode.nix {})
 		(callPackage ./greeter/games/no-mans-sky.nix {})
 
