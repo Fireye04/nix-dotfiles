073cf36a0cb093834cdba5e2401f9281615aef1e
Fireye <codekai16@gmail.com>
2026-01-20 13:10:41 -0700

    rev

---
+++ root/utils/nvimunity/default.nix
@@ -0,0 +1,27 @@
+{
+	lib,
+	makeWrapper,
+	runCommand,
+	# runtime deps (depend on script)
+	bash,
+}: let
+	# Your external shell script.
+	src = ./nvimunity.sh;
+	binName = "nvimunity";
+	deps = [
+		bash
+	];
+in
+	runCommand "${binName}"
+	{
+		nativeBuildInputs = [makeWrapper];
+		meta = {
+			mainProgram = "${binName}";
+		};
+	}
+	''
+		mkdir -p $out/bin
+		install -m +x ${src} $out/bin/${binName}
+		wrapProgram $out/bin/${binName} \
+		  --prefix PATH : ${lib.makeBinPath deps}
+	''
