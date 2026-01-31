a3d98280df473af1937b1d2f4205b04d8fbde4a2
Fireye <codekai16@gmail.com>
2026-01-30 17:15:32 -0700

    rev

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -92,10 +92,10 @@
 			  // for the resolution.
 			  // If the mode is omitted altogether or is invalid, niri will pick one automatically.
 			  // Run `niri msg outputs` while inside a niri instance to list all outputs and their modes.
-			  mode "1920x1080@59.977"
+			  // mode "1920x1080@59.977"
 
 			  // You can use integer or fractional scale, for example use 1.5 for 150% scale.
-			  scale 1
+			  scale 1.5
 
 			  // Transform allows to rotate the output counter-clockwise, valid values are:
 			  // normal, 90, 180, 270, flipped, flipped-90, flipped-180 and flipped-270.
@@ -429,7 +429,7 @@
 			match app-id=r#"^kitty$"# is-floating=true
 			opacity 0.8
 			}
-		 
+				 
 			window-rule {
 			match app-id=r#"^kitty$"# title="^peaclock$"
 			opacity 0.8
