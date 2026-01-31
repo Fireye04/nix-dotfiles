de8d2d7bc7aba3075fe0007e8efbf51927ecee8b
Fireye <codekai16@gmail.com>
2026-01-31 01:21:08 -0700

    rev

---
+++ home/noctalia/default.nix
@@ -0,0 +1,16 @@
+{
+	config,
+	pkgs,
+	inputs,
+}: {
+	# import the home manager module
+	imports = [
+		inputs.noctalia.homeModules.default
+	];
+
+	# configure options
+	programs.noctalia-shell = {
+		enable = true;
+		settings = ./config.json;
+	};
+}
