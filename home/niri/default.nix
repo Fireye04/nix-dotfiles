37adafaaf174fe54f06e6730ef3b98e0d048e04a
Fireye <codekai16@gmail.com>
2026-02-03 14:48:16 -0700

    rev

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -304,8 +304,8 @@
 			//spawn-at-startup "wbg" "/usr/share/backgrounds/waterfall.png"
 			// spawn-at-startup "sleep" "1;" "swww" "img" "/etc/nixos/wallpapers/green_cabin.jpg" "--transition-type" "center"
 			spawn-at-startup "~/.config/swww/bin/randomize_multi.sh" "/etc/nixos/wallpapers"
-			spawn-at-startup "kitty" "spotify_player"
-			spawn-at-startup "thunderbird"
+			// spawn-at-startup "kitty" "spotify_player"
+			// spawn-at-startup "thunderbird"
 
 			environment {
 			  DISPLAY ":0"
@@ -429,7 +429,7 @@
 			match app-id=r#"^kitty$"# is-floating=true
 			opacity 0.8
 			}
-			 
+		 
 			window-rule {
 			match app-id=r#"^kitty$"# title="^peaclock$"
 			opacity 0.8
