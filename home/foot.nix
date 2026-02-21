cbfec8dabcfc3362532ea7d437b0f6f8dd41169a
Fireye <codekai16@gmail.com>
2026-02-21 16:03:31 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -3,6 +3,14 @@
 	pkgs,
 }: {
 	programs.foot = {
+		package =
+			pkgs.foot.overrideAttrs (oldAttrs: {
+					src =
+						pkgs.fetchGit {
+							url = "https://codeberg.org/dnkl/foot";
+							rev = "e63150305ebe394960fbe386bed961f6aa5202dc";
+						};
+				});
 		enable = true;
 		settings = {
 			main = {
