8210737e9fc24674620936d6af6825635c9ff21b
Fireye <codekai16@gmail.com>
2026-02-21 16:23:24 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -3,20 +3,11 @@
 	pkgs,
 }: {
 	programs.foot = {
-		package =
-			pkgs.foot.overrideAttrs (oldAttrs: {
-					src =
-						pkgs.fetchFromCodeberg {
-							owner = "dnkl";
-							repo = "foot";
-							rev = "e63150305ebe394960fbe386bed961f6aa5202dc";
-							hash = "sha256-mhjRqZcGkE/QGS3a7H5Veo/Ou95/5kZt54WUEFffK6w=";
-						};
-				});
 		enable = true;
 		settings = {
 			main = {
 				font = "Pixel Code:size=13";
+				blur = "yes";
 			};
 			cursor = {
 				style = "underline";
