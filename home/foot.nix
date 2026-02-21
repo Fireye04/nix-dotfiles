0eab3f38d34c1bedf8bd96f8b33a83874d1216f4
Fireye <codekai16@gmail.com>
2026-02-21 16:05:00 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -9,6 +9,7 @@
 						pkgs.fetchgit {
 							url = "https://codeberg.org/dnkl/foot";
 							rev = "e63150305ebe394960fbe386bed961f6aa5202dc";
+							hash = "sha256-mhjRqZcGkE/QGS3a7H5Veo/Ou95/5kZt54WUEFffK6w=";
 						};
 				});
 		enable = true;
