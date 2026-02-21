44f37f856c0f37e09078fafa8c952336a18ebf52
Fireye <codekai16@gmail.com>
2026-02-21 16:08:05 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -6,8 +6,9 @@
 		package =
 			pkgs.foot.overrideAttrs (oldAttrs: {
 					src =
-						pkgs.fetchgit {
-							url = "https://codeberg.org/dnkl/foot";
+						pkgs.fetchFromCodeberg {
+							owner = "dnkl";
+							repo = "foot";
 							rev = "e63150305ebe394960fbe386bed961f6aa5202dc";
 							hash = "sha256-mhjRqZcGkE/QGS3a7H5Veo/Ou95/5kZt54WUEFffK6w=";
 						};
