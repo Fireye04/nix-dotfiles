b8af611aa465951352d592308de7ead13e06b3ba
Fireye <codekai16@gmail.com>
2026-02-21 16:56:23 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -3,11 +3,20 @@
 	pkgs,
 }: {
 	programs.foot = {
+		package =
+			pkgs.foot.overrideAttrs (oldAttrs: {
+					src =
+						pkgs.fetchFromCodeberg {
+							owner = "Fireye";
+							repo = "foot";
+							rev = "efe40d460b495f6d5c2448eadcd3480002d4f12c";
+							hash = "sha256-mhjRqZcGkE/QGS3a7H5Veo/Ou95/5kZt54WUEFffK6w=";
+						};
+				});
 		enable = true;
 		settings = {
 			main = {
 				font = "Pixel Code:size=13";
-				blur = "yes";
 			};
 			cursor = {
 				style = "underline";
