abcff6ebba5a4ddd060aa5c4414b83c6ae7bbf4f
Fireye <codekai16@gmail.com>
2026-02-21 17:07:16 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -11,7 +11,6 @@
 							owner = "dnkl";
 							repo = "foot";
 							version = "1.25.0";
-							hash = "sha256-mhjRqZcGkE/QGS3a7H5Veo/Ou95/5kZt54WUEFffK6w=";
 						};
 				});
 		enable = true;
