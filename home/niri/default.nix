ddd1903364c2919486f53b193951557d1ac6b8fe
Fireye <codekai16@gmail.com>
2026-02-23 14:07:02 -0700

    rev

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -422,6 +422,9 @@
 			window-rule {
 			match app-id=r#"^kitty$"# title="^cmatrix$"
 			opacity 0.6
+			         background-effect {
+			     blur true
+			 }
 			}
 
 			window-rule {
@@ -433,7 +436,7 @@
 			match app-id=r#"^kitty$"# is-floating=true
 			opacity 0.8
 			}
-		 
+				 
 			window-rule {
 			match app-id=r#"^kitty$"# title="^peaclock$"
 			opacity 0.8
