a66e0e399ae182bbdddf7b516ebe0cafc4ea4554
Fireye <codekai16@gmail.com>
2026-02-23 02:33:44 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -4,18 +4,7 @@
 	pkgs-stable,
 }: {
 	programs.foot = {
-		package =
-			pkgs.foot.overrideAttrs
-			(oldAttrs: {
-					src =
-						pkgs.fetchFromCodeberg {
-							owner = "Fireye";
-							repo = "foot";
-							tag = "1.25.0b";
-							hash = "sha256-s7SwIdkWhBKcq9u4V0FLKW6CA36MBvDyB9ELB0V52O0=";
-						};
-				});
-		enable = false;
+		enable = true;
 		settings = {
 			main = {
 				font = "Pixel Code:size=13";
