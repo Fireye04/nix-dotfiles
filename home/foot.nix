04218964e8cf3c1215ce9924673e689e87ad154a
Fireye <codekai16@gmail.com>
2026-02-23 02:40:21 -0700

    rev

--- home/foot.nix
+++ home/foot.nix
@@ -4,6 +4,17 @@
 	pkgs-stable,
 }: {
 	programs.foot = {
+		package =
+			pkgs.foot.overrideAttrs
+			(oldAttrs: {
+					src =
+						pkgs.fetchFromCodeberg {
+							owner = "dnkl";
+							repo = "foot";
+							rev = "e63150305ebe394960fbe386bed961f6aa5202dc";
+							hash = "sha256-s7SwIdkWhBKcq9u4V0FLKW6CA36MBvDyB9ELB0V52O0=";
+						};
+				});
 		enable = true;
 		settings = {
 			main = {
