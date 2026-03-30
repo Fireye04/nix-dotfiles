f883e26a83830dfa0106d162442c6cbda5a1a3de
Fireye <codekai16@gmail.com>
2026-03-29 23:11:02 -0700

    rev

--- root/default.nix
+++ root/default.nix
@@ -258,5 +258,5 @@
 	# this value at the release version of the first install of this system.
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	pkgs.stdenv.hostPlatform.system.stateVersion = "25.05"; # Did you read the comment?
+	stdenv.hostPlatform.system.stateVersion = "25.05"; # Did you read the comment?
 }
