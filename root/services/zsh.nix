5b487ca0d24a39a70f6b16a199bd5fd6c1d1a01d
Fireye <codekai16@gmail.com>
2026-04-03 14:01:29 -0700

    rev

--- root/services/zsh.nix
+++ root/services/zsh.nix
@@ -11,7 +11,7 @@
 			ninit = "cd /etc/nixos;nvim .";
 			update = "sudo nixos-rebuild switch";
 			gupdate = "git add --all;git commit -m 'rev';git push;sudo nixos-rebuild switch";
-			nixupdate = "git branch -D update;git checkout -b update;git add --all;git commit -m 'update';git push;nix flake update;sudo nixos-rebuild switch";
+			nixupdate = "git branch -D update;nix flake update;git checkout -b update;git add --all;git commit -m 'update';git push;sudo nixos-rebuild switch";
 			gaa = "git add --all";
 			gc = "git commit";
 			gp = "git push";
