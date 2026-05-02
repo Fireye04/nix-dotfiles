bbf7487f9fb32460d1063aeb1cd9c94d60b465ef
Fireye <codekai16@gmail.com>
2026-05-01 20:23:55 -0700

    rev

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 		niri = {
 			url = "github:sodiboo/niri-flake";
-			cache.enable = true;
+			cache = true;
 		};
 		fix-python.url = "github:GuillaumeDesforges/fix-python";
 		nix-alien.url = "github:thiagokokada/nix-alien";
