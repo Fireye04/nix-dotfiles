f115a4f8a8402c51bb6a2528966994d7a4163f85
Fireye <codekai16@gmail.com>
2026-05-19 12:49:57 -0700

    rev

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 		# 	inputs.nixpkgs.follows = "nixpkgs";
 		# };
 
-		gaypanel.url = "https://codeberg.org/Fireye/gaypanel";
+		gaypanel.url = "https://codeberg.org/Fireye/gaypanel.git";
 
 		stylix = {
 			url = "github:nix-community/stylix";
