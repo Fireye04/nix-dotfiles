dbf9b566e8f375986cfcce499b721b28006dd015
Fireye <codekai16@gmail.com>
2026-06-09 22:39:33 -0700

    update

--- root/greeter/default.nix
+++ root/greeter/default.nix
@@ -19,7 +19,7 @@
 		enable = true;
 	};
 	programs.sway.enable = true;
-	services.desktopManager.cosmic.enable = true;
+	# services.desktopManager.cosmic.enable = true;
 	services.greetd = {
 		enable = true;
 	};
