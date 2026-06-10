2fd65224361068edf546dd4e913918254e1bffde
Fireye <codekai16@gmail.com>
2026-06-10 12:44:22 -0700

    rev

--- home/default.nix
+++ home/default.nix
@@ -77,6 +77,7 @@
 		unzip
 		gram
 		zig
+		ziggy
 		pdftk
 		img2pdf
 		udiskie
@@ -255,7 +256,7 @@
 
 	home.file = {
 		".gitconfig-work" = {
-			text = ''							
+			text = ''						
 				[user]
 					email = kkoehler@lsst.org
 					name = Kai Koehler'';
