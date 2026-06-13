96a320c3066791115e11616e4f78cff844918765
Fireye <codekai16@gmail.com>
2026-06-12 17:28:01 -0700

    rev

--- root/greeter/default.nix
+++ root/greeter/default.nix
@@ -26,46 +26,46 @@
 			command = "dbus-run-session cage -s -mlast -d -- regreet";
 		};
 	};
-	# programs.regreet = {
-	# 	enable = true;
-	# 	cageArgs = ["-s" "-m" "last"];
-	# 	cursorTheme = {
-	# 		name = "Bibata-Modern-Classic";
-	# 		package = pkgs.bibata-cursors;
-	# 	};
-	# 	font = {
-	# 		name = "JetBrainsMonoNerdFont";
-	# 		size = 16;
-	# 		package = pkgs.nerd-fonts.jetbrains-mono;
-	# 	};
-	# 	theme = {
-	# 		name = "Canta-dark";
-	# 		package = pkgs.canta-theme;
-	# 	};
-	# 	settings = {
-	# 		background = {
-	# 			# Path to the background image
-	# 			path = "/etc/nixos/wallpapers/green_cabin.jpg";
-	#
-	# 			# How the background image covers the screen if the aspect ratio doesn't match
-	# 			# Available values: "Fill", "Contain", "Cover", "ScaleDown"
-	# 			# Refer to: https://docs.gtk.org/gtk4/enum.ContentFit.html
-	# 			# NOTE: This is ignored if ReGreet isn't compiled with GTK v4.8 support.
-	# 			fit = "Contain";
-	# 		};
-	# 		gtk = {
-	# 			application_prefer_dark_theme = true;
-	# 		};
-	# 		appearance = {
-	# 			greeting_msg = "Climb a tree! :3";
-	# 		};
-	#
-	# 		commands = {
-	# 			#BREAKS x11 SESSIONS (probably)
-	# 			x11_prefix = [""];
-	# 		};
-	# 	};
-	# };
+	programs.regreet = {
+		enable = true;
+		cageArgs = ["-s" "-m" "last"];
+		cursorTheme = {
+			name = "Bibata-Modern-Classic";
+			package = pkgs.bibata-cursors;
+		};
+		font = {
+			name = "JetBrainsMonoNerdFont";
+			size = 16;
+			package = pkgs.nerd-fonts.jetbrains-mono;
+		};
+		theme = {
+			name = "Canta-dark";
+			package = pkgs.canta-theme;
+		};
+		settings = {
+			background = {
+				# Path to the background image
+				path = "/etc/nixos/wallpapers/green_cabin.jpg";
+
+				# How the background image covers the screen if the aspect ratio doesn't match
+				# Available values: "Fill", "Contain", "Cover", "ScaleDown"
+				# Refer to: https://docs.gtk.org/gtk4/enum.ContentFit.html
+				# NOTE: This is ignored if ReGreet isn't compiled with GTK v4.8 support.
+				fit = "Contain";
+			};
+			gtk = {
+				application_prefer_dark_theme = true;
+			};
+			appearance = {
+				greeting_msg = "Climb a tree! :3";
+			};
+
+			commands = {
+				#BREAKS x11 SESSIONS (probably)
+				x11_prefix = [""];
+			};
+		};
+	};
 
 	services.xserver.displayManager.session = [
 		# {
