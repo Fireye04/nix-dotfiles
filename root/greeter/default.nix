c3e489b777aed40a494ab16835ef107405f74ba0
Fireye <codekai16@gmail.com>
2026-06-14 01:02:16 -0700

    rev

--- root/greeter/default.nix
+++ root/greeter/default.nix
@@ -29,7 +29,7 @@
 	# };
 	programs.regreet = {
 		enable = true;
-		package = pkgs-stable.regreet;
+		# package = pkgs-stable.regreet;
 		cageArgs = ["-s" "-m" "-d" "last"];
 		# cursorTheme = {
 		# 	name = "Bibata-Modern-Classic";
