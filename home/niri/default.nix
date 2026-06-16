efaa9bbe24b5dd15d00ff538528b74d56b981ef3
Fireye <codekai16@gmail.com>
2026-06-16 09:20:55 -0700

    rev

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -83,7 +83,7 @@
 			}
 
 			         output "Dell Inc. DELL P2412H KG49T24F2HJU" {
-			           position x=-100 y=-1080
+			           position x=-50 y=-1080
 			         }
 
 			output "eDP-1" {
@@ -448,7 +448,7 @@
 			     blur true
 			 }
 			}
-			 
+		 
 			window-rule {
 			match app-id=r#"^foot$"# title="^peaclock$"
 			opacity 0.8
@@ -456,7 +456,7 @@
 			     blur true
 			 }
 			}
-		 
+				 
 			window-rule {
 			match app-id=r#"^foot$"# title="^Close OS window$"
 			opacity 0.5
